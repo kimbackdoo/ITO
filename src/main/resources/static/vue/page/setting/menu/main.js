f4a7f83fbc761055f8cdd962e7774644222ca75a
smpark <smpark@meta-soft.co.kr>
2021-03-19 16:24:26 +0900

    메뉴 버그 수정

--- src/main/resources/static/vue/page/setting/menu/main.js
+++ src/main/resources/static/vue/page/setting/menu/main.js
@@ -40,7 +40,6 @@ SettingMenuMainPage = Vue.component("setting-menu-main-page", async function (re
         },
         "treeview.menuList.active": {
             "handler": function (newValue, oldValue) {
-                this.data.menu.
                 this.data.menu = newValue[0] ? _.cloneDeep(newValue[0]) : {"show": null};
             }
         }
