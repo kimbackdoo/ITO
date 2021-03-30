fa3486ee3d2a74a6eee78d54990c67539594a7c9
박진수 <@DESKTOP-RLBLQK0>
2021-03-30 09:12:26 +0900

    "ProjectPersonDto , ProjectPersonParamDto" 생성

---
+++ src/main/java/kr/co/metasoft/ito/api/common/dto/ProjectPersonDto.java
@@ -0,0 +1,23 @@
+package kr.co.metasoft.ito.api.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProjectPersonDto{
+
+    private Long userId;
+
+    private Long personId;
+
+
+}
