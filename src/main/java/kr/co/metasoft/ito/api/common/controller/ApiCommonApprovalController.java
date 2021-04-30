afef4c42cef7e1e0b1384b5857bd8a907a2b60b2
박진수 <@DESKTOP-RLBLQK0>
2021-04-30 15:48:38 +0900

    "modify 수정"

--- src/main/java/kr/co/metasoft/ito/api/common/controller/ApiCommonApprovalController.java
+++ src/main/java/kr/co/metasoft/ito/api/common/controller/ApiCommonApprovalController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,7 +61,7 @@ public class ApiCommonApprovalController {
         return approvalService.createApprovalEntity(approvalEntity);
     }
 
-    @PostMapping(path = "{id}") //vacationId
+    @PutMapping(path = "{id}") //vacationId
     public ApprovalEntity modifyApproval(
             @PathVariable (name = "id") Long id,
             @RequestBody ApprovalDto approvalDto) {
