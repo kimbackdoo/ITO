f158806a603d3c6765531d650c33687d7cf948e6
박진수 <@DESKTOP-RLBLQK0>
2021-04-30 16:23:12 +0900

    ""

--- src/main/java/kr/co/metasoft/ito/api/common/entity/ApprovalEntity.java
+++ src/main/java/kr/co/metasoft/ito/api/common/entity/ApprovalEntity.java
@@ -47,6 +47,7 @@ public class ApprovalEntity {
     @Column (name = "`id`", columnDefinition = "bigint(20)")
     private Long id;
 
+
     @Column (name = "`vacation_id`", columnDefinition = "bigint(20)")
     private Long vacationId;
 
